378c0e820e09c20ce9d18134efab0a6eca9dd5ef
Maxim Dobryakov <maxim.dobryakov@gmail.com>
2020-09-11 22:43:46 +0300

    Add error handling to CMake packages

--- cmake/FindEspIdf.cmake
+++ cmake/FindEspIdf.cmake
@@ -1,14 +1,21 @@
-SET(ESP_IDF_PATH $ENV{HOME}/.esp-idf)
+IF (NOT DEFINED ENV{IDF_PATH})
+    SET(ESP_IDF_PATH $ENV{HOME}/.esp-idf)
 
-# Set env variable with ESP IDF path
-SET(ENV{IDF_PATH} ${ESP_IDF_PATH})
+    IF (NOT EXISTS ${ESP_IDF_PATH})
+        message(FATAL_ERROR "'${ESP_IDF_PATH}' directory with ESP-IDF framework not found. "
+                "See 'Step 2. Get ESP-IDF' by link https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/index.html#step-2-get-esp-idf")
+    ENDIF()
 
-# Set additional look-up paths (i.e. with ESP-IDF tools)
-LIST(APPEND CMAKE_SYSTEM_PROGRAM_PATH
-        "${ESP_IDF_PATH}/components/esptool_py/esptool"
-        "${ESP_IDF_PATH}/components/espcoredump"
-        "${ESP_IDF_PATH}/components/partition_table/"
-        "${ESP_IDF_PATH}/components/esptool_py/esptool"
-        "${ESP_IDF_PATH}/components/espcoredump"
-        "${ESP_IDF_PATH}/components/partition_table/"
-        "${ESP_IDF_PATH}/tools")
+    # Set env variable with ESP IDF path
+    SET(ENV{IDF_PATH} ${ESP_IDF_PATH})
+
+    # Set additional look-up paths (i.e. with ESP-IDF tools)
+    LIST(APPEND CMAKE_SYSTEM_PROGRAM_PATH
+            "${ESP_IDF_PATH}/components/esptool_py/esptool"
+            "${ESP_IDF_PATH}/components/espcoredump"
+            "${ESP_IDF_PATH}/components/partition_table/"
+            "${ESP_IDF_PATH}/components/esptool_py/esptool"
+            "${ESP_IDF_PATH}/components/espcoredump"
+            "${ESP_IDF_PATH}/components/partition_table/"
+            "${ESP_IDF_PATH}/tools")
+ENDIF()
--- cmake/FindEspressif.cmake
+++ cmake/FindEspressif.cmake
@@ -1,23 +1,30 @@
-SET(ESPRESSIF_PATH $ENV{HOME}/.espressif)
+IF (NOT DEFINED ENV{IDF_PYTHON_ENV_PATH})
+    SET(ESPRESSIF_PATH $ENV{HOME}/.espressif)
 
-# Find Python environment directory
-file(GLOB PYTHON_ENV_DIR "${ESPRESSIF_PATH}/python_env/*")
+    IF (NOT EXISTS ${ESPRESSIF_PATH})
+        message(FATAL_ERROR "'${ESPRESSIF_PATH}' directory with Espressif tools not found. "
+                "See 'Step 3. Set up the tools' by link https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/index.html#id3")
+    ENDIF()
 
-file(GLOB XTENSA_ESP32_ELF_DIR "${ESPRESSIF_PATH}/tools/xtensa-esp32-elf/*/xtensa-esp32-elf")
-file(GLOB XTENSA_ESP32S2_ELF_DIR "${ESPRESSIF_PATH}/tools/xtensa-esp32s2-elf/*/xtensa-esp32s2-elf")
-file(GLOB XESP32ULP_ELF_DIR "${ESPRESSIF_PATH}/tools/esp32ulp-elf/*/esp32ulp-elf-binutils")
-file(GLOB XESP32S2ULP_ELF_DIR "${ESPRESSIF_PATH}/tools/esp32s2ulp-elf/*/esp32s2ulp-elf-binutils")
-file(GLOB OPENOCD_ESP32_DIR "${ESPRESSIF_PATH}/tools/openocd-esp32/*/openocd-esp32")
+    # Find Python environment directory
+    file(GLOB PYTHON_ENV_DIR "${ESPRESSIF_PATH}/python_env/*")
 
-# Set env variables with Python environment directory and executable Python file
-SET(ENV{IDF_PYTHON_ENV_PATH} ${PYTHON_ENV_DIR})
-SET(ENV{PYTHON} ${PYTHON_ENV_DIR}/bin/python)
+    file(GLOB XTENSA_ESP32_ELF_DIR "${ESPRESSIF_PATH}/tools/xtensa-esp32-elf/*/xtensa-esp32-elf")
+    file(GLOB XTENSA_ESP32S2_ELF_DIR "${ESPRESSIF_PATH}/tools/xtensa-esp32s2-elf/*/xtensa-esp32s2-elf")
+    file(GLOB XESP32ULP_ELF_DIR "${ESPRESSIF_PATH}/tools/esp32ulp-elf/*/esp32ulp-elf-binutils")
+    file(GLOB XESP32S2ULP_ELF_DIR "${ESPRESSIF_PATH}/tools/esp32s2ulp-elf/*/esp32s2ulp-elf-binutils")
+    file(GLOB OPENOCD_ESP32_DIR "${ESPRESSIF_PATH}/tools/openocd-esp32/*/openocd-esp32")
 
-# Set additional look-up paths (i.e. with C/CPP compilers, etc.)
-LIST(APPEND CMAKE_SYSTEM_PROGRAM_PATH
-        "${XTENSA_ESP32_ELF_DIR}/bin"
-        "${XTENSA_ESP32S2_ELF_DIR}/bin"
-        "${XESP32ULP_ELF_DIR}/bin"
-        "${XESP32S2ULP_ELF_DIR}/bin"
-        "${OPENOCD_ESP32_DIR}/bin"
-        "${PYTHON_ENV_DIR}/bin")
+    # Set env variables with Python environment directory and executable Python file
+    SET(ENV{IDF_PYTHON_ENV_PATH} ${PYTHON_ENV_DIR})
+    SET(ENV{PYTHON} ${PYTHON_ENV_DIR}/bin/python)
+
+    # Set additional look-up paths (i.e. with C/CPP compilers, etc.)
+    LIST(APPEND CMAKE_SYSTEM_PROGRAM_PATH
+            "${XTENSA_ESP32_ELF_DIR}/bin"
+            "${XTENSA_ESP32S2_ELF_DIR}/bin"
+            "${XESP32ULP_ELF_DIR}/bin"
+            "${XESP32S2ULP_ELF_DIR}/bin"
+            "${OPENOCD_ESP32_DIR}/bin"
+            "${PYTHON_ENV_DIR}/bin")
+ENDIF()
